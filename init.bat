c20c717f17bbf03cba7e4c9ce39f79247dc4a7af
wling-art <wlingzhenyu@163.com>
2025-05-28 15:43:25 +0800

    直接运行 uv 命令

--- init.bat
+++ init.bat
@@ -1,12 +1,2 @@
-if EXIST .\venv\(
-    echo "creating venv"
-    pip install uv
-    uv venv
-    uv sync
-)
-else(
-    echo "venv exists"
-    uv sync
-    source .\venv\Scripts\activate.bat
-    python3 ./bot.py
-)
+uv sync
+uv run bot.py
--- init.sh
+++ init.sh
@@ -1,13 +1,2 @@
-if [ ! -d .venv ]; then
-echo "creating venv"
-pip install uv
-uv venv
 uv sync
-
-else
-    echo "venv already exists"
-    uv sync
-    source .venv/bin/activate
-    echo "venv activated"
-    python3 ./bot.py
-fi
+uv run bot.py
