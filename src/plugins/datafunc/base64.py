dfc8fbe199a0e6643a537a308e6cc6ab43e4ac1b
wling-art <wlingzhenyu@163.com>
2025-05-29 16:53:41 +0800

    优化Base64命令的错误处理逻辑，统一使用matcher.finish()发送消息

--- src/plugins/datafunc/base64.py
+++ src/plugins/datafunc/base64.py
@@ -22,7 +22,7 @@ async def base64_runner(matcher: Matcher, args: Message = CommandArg()):
     if location := args.extract_plain_text():
         location = location.strip().split(maxsplit=1)
         logger.debug(location)
-        if location[0].lower() != "decode" and location[0].lower() != "encode":
+        if location[0].lower() not in ["decode", "encode"]:
             await matcher.send("请输入正确选项！可用的：decode encode")
             return
         if len(location) > 1:
@@ -31,17 +31,17 @@ async def base64_runner(matcher: Matcher, args: Message = CommandArg()):
                     finish = base64.b64decode(location[1].encode("utf-8"))
                     message = None
                     message = str(finish.decode("utf-8"))
-                    await matcher.send(str(message))
+                    await matcher.finish(message)
                 except binascii.Error:
-                    await matcher.send("不合法的Base64格式！")
+                    await matcher.finish("不合法的Base64格式！")
                 except ValueError:
-                    await matcher.send("不合法的Base64格式（非4倍数位）！")
+                    await matcher.finish("不合法的Base64格式（非4倍数位）！")
             elif location[0].lower() == "encode":
                 finish = base64.b64encode(location[1].encode("utf-8"))
                 message = str(finish.decode("utf-8"))
-                await matcher.send(message)
+                await matcher.finish(message)
             else:
-                await matcher.send("输入正确选项！可用的：decode encode")
+                await matcher.finish("输入正确选项！可用的：decode encode")
         else:
             await matcher.send("请输入文本！")
     else:
