a164625ae3f8f6209278d0bc4b96424f36e9e217
wling-art <wlingzhenyu@163.com>
2025-06-01 14:09:37 +0800

    [Feat] 支持菜单打印

--- litebot_utils/utils.py
+++ litebot_utils/utils.py
@@ -1,7 +1,8 @@
-
 import nonebot
 from nonebot import logger
 from nonebot.adapters.onebot.v11 import Bot
+from nonebot.adapters.onebot.v11.event import GroupMessageEvent, MessageEvent
+from nonebot.adapters.onebot.v11.message import MessageSegment
 
 
 async def send_to_admin(message):
@@ -15,3 +16,35 @@ async def send_to_admin(message):
     if isinstance(bot, Bot):
         await bot.send_group_msg(group_id=966016220, message=message)
     logger.info(f"Sending to admin: {message}")
+
+
+async def send_forward_msg(
+    bot: Bot,
+    event: MessageEvent,
+    name: str,
+    uin: str,
+    msgs: list[MessageSegment],
+) -> dict:
+    """
+    发送转发消息的异步函数。
+
+    参数:
+        bot (Bot): 机器人实例
+        event (MessageEvent): 消息事件
+        name (str): 转发消息的名称
+        uin (str): 转发消息的 UIN
+        msgs (list[Message]): 转发的消息列表
+
+    返回:
+        dict: API 调用结果
+    """
+
+    def to_json(msg: MessageSegment) -> dict:
+        return {"type": "node", "data": {"name": name, "uin": uin, "content": msg}}
+
+    messages = [to_json(msg) for msg in msgs]
+    if isinstance(event, GroupMessageEvent):
+        return await bot.send_group_forward_msg(
+            group_id=event.group_id, messages=messages
+        )
+    return await bot.send_private_forward_msg(user_id=event.user_id, messages=messages)
