b5cbcc1df10d2f2259f136aa2cff11ee5b21b13b
wling-art <wlingzhenyu@163.com>
2025-06-02 12:50:03 +0800

    修复黑名单检查的异步调用，确保正确处理好友和群组请求

--- src/plugins/manager/add.py
+++ src/plugins/manager/add.py
@@ -5,6 +5,7 @@ from nonebot.adapters.onebot.v11 import (
     GroupRequestEvent,
     RequestEvent,
 )
+
 from litebot_utils.blacklist.black import bl_manager
 from litebot_utils.utils import send_to_admin
 
@@ -12,19 +13,23 @@ from litebot_utils.utils import send_to_admin
 @on_request().handle()
 async def _(event: RequestEvent, bot: Bot):
     if isinstance(event, FriendRequestEvent):
-        if bl_manager.is_private_black(str(event.user_id)):
+        if await bl_manager.is_private_black(str(event.user_id)):
             await send_to_admin(f"尝试拒绝添加黑名单用户{event.user_id}.......")
             await event.reject(bot)
             return
         await send_to_admin(f"收到{event.user_id}的好友请求：{event.comment or ''}")
         await event.approve(bot=bot)
     if isinstance(event, GroupRequestEvent):
-        if bl_manager.is_private_black(str(event.user_id)):
-            await send_to_admin(f"尝试拒绝添加黑名单用户{event.user_id}的拉群请求.......")
+        if await bl_manager.is_private_black(str(event.user_id)):
+            await send_to_admin(
+                f"尝试拒绝添加黑名单用户{event.user_id}的拉群请求......."
+            )
             await event.reject(bot)
             return
-        if bl_manager.is_group_black(str(event.group_id)):
-            await send_to_admin(f"尝试拒绝添加黑名单群组{event.group_id}的拉群请求.......")
+        if await bl_manager.is_group_black(str(event.group_id)):
+            await send_to_admin(
+                f"尝试拒绝添加黑名单群组{event.group_id}的拉群请求......."
+            )
             await event.reject(bot)
             return
         flag = event.flag
