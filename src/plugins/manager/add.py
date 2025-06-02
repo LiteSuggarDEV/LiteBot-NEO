c6d5fae13a3abbc3b58fb8321f64fa89b3cfa73e
John Richard <67693593+JohnRichard4096@users.noreply.github.com>
2025-06-02 13:14:45 +0800

    [Fix] 修复退群后还发送消息的问题,修复配置文件未创建问题 (#40)
    * [Patch]修复退群后还发送消息的问题
    
    * 添加配置文件支持，使用 YAML 格式存储配置，移除不必要的依赖
    
    * 格式化代码
    
    * 在重新加载配置时添加日志记录
    
    ---------
    
    Co-authored-by: wling-art <wlingzhenyu@163.com>

--- src/plugins/manager/add.py
+++ src/plugins/manager/add.py
@@ -5,6 +5,7 @@ from nonebot.adapters.onebot.v11 import (
     GroupRequestEvent,
     RequestEvent,
 )
+
 from litebot_utils.blacklist.black import bl_manager
 from litebot_utils.utils import send_to_admin
 
@@ -20,11 +21,15 @@ async def _(event: RequestEvent, bot: Bot):
         await event.approve(bot=bot)
     if isinstance(event, GroupRequestEvent):
         if bl_manager.is_private_black(str(event.user_id)):
-            await send_to_admin(f"尝试拒绝添加黑名单用户{event.user_id}的拉群请求.......")
+            await send_to_admin(
+                f"尝试拒绝添加黑名单用户{event.user_id}的拉群请求......."
+            )
             await event.reject(bot)
             return
         if bl_manager.is_group_black(str(event.group_id)):
-            await send_to_admin(f"尝试拒绝添加黑名单群组{event.group_id}的拉群请求.......")
+            await send_to_admin(
+                f"尝试拒绝添加黑名单群组{event.group_id}的拉群请求......."
+            )
             await event.reject(bot)
             return
         flag = event.flag
