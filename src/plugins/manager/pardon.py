ef662f881e338161834be315a42dbda79c812cfe
MrlingXD <90316914+wling-art@users.noreply.github.com>
2025-06-02 22:26:42 +0800

    [fix/patch] 优化封禁和解封命令的参数验证 (#50)
    * 优化封禁和解封命令的参数验证
    
    * fix: 解封命令中的参数错误

--- src/plugins/manager/pardon.py
+++ src/plugins/manager/pardon.py
@@ -25,23 +25,23 @@ pardon_user = pardon.command(
 
 @pardon_group.handle()
 async def _(args: Message = CommandArg()):
-    arg_list = args.extract_plain_text().strip()
-    if not arg_list and len(arg_list) <= 2:
+    arg = args.extract_plain_text().strip()
+    if not arg:
         await pardon_group.finish("请提供要解封的群ID！")
-    if not await bl_manager.is_group_black(arg_list):
+    if not await bl_manager.is_group_black(arg):
         await pardon_group.finish("该群未被封禁！")
     else:
-        await bl_manager.group_remove(arg_list)
-        await pardon_group.finish(f"解封禁群{arg_list}成功！")
+        await bl_manager.group_remove(arg)
+        await pardon_group.finish(f"解封禁群{arg}成功！")
 
 
 @pardon_user.handle()
 async def pardon_user_handle(args: Message = CommandArg()):
-    arg_list = args.extract_plain_text().strip()
-    if not arg_list and len(arg_list) <= 2:
+    arg = args.extract_plain_text().strip()
+    if not arg:
         await pardon_group.finish("请提供要解封的用户ID！")
-    if not await bl_manager.is_private_black(arg_list):
+    if not await bl_manager.is_private_black(arg):
         await pardon_user.finish("该用户没有被封禁！")
     else:
-        await bl_manager.private_remove(arg_list)
-        await pardon_user.finish(f"解封禁用户{arg_list}成功！")
+        await bl_manager.private_remove(arg)
+        await pardon_user.finish(f"解封禁用户{arg}成功！")
