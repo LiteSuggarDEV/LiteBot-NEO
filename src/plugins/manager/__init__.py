95d2148496def62d95b0ad2e1087542ff873bc5e
John Richard <windowserror@163.com>
2025-06-14 14:59:31 +0800

    更改描述

--- src/plugins/manager/__init__.py
+++ src/plugins/manager/__init__.py
@@ -4,7 +4,7 @@ from . import add, auto_clean, ban, black, leave, pardon, rate
 
 __plugin_meta__ = PluginMetadata(
     name="管理插件",
-    description="管理器",
+    description="管理器（TO管理员：您的每一个操作都会让用户发出尖锐的爆鸣声）",
     usage="管理器插件",
     type="application",
 )
