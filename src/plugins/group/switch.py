071198be64996e6c9c8dd90d0d8f61114d360a36
John Richard <windowserror@163.com>
2025-06-14 16:33:38 +0800

    移除不必要的command start

--- src/plugins/group/switch.py
+++ src/plugins/group/switch.py
@@ -13,7 +13,7 @@ from src.plugins.menu.manager import MatcherData
 
 command_start = get_driver().config.command_start
 switch = on_command(
-    tuple(f"{prefix}switch" for prefix in command_start),
+    "switch",
     rule=is_group_admin,
     state=MatcherData(
         rm_name="切换LiteBot启用状态",
